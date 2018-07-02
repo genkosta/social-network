8678fea89a8aad319aef23d78e9454b9f9c539da
genkosta <genkosta43@gmail.com>
2018-07-02 17:22:24 +0300

    Update API - Add test LikePostTest

--- posts/tests.py
+++ posts/tests.py
@@ -101,3 +101,26 @@ class DeletePostTest(APITestCase):
         url = f'/api/v1/integrations/posts/{self.post.id}/'
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class LikePostTest(APITestCase):
+
+    def setUp(self):
+        self.superuser = User.objects.create_superuser('admin', 'admin@site.net', '1234')
+        self.client.login(username='admin', password='1234')
+        data = {
+            'user': self.superuser,
+            'title': 'Test Post',
+            'message': 'Hello world!'
+        }
+        self.post = Post.objects.create(**data)
+
+    def test_can_like_post(self):
+        url = f'/api/v1/integrations/posts/{self.post.id}/like/'
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_unlike_post(self):
+        url = f'/api/v1/integrations/posts/{self.post.id}/unlike/'
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
